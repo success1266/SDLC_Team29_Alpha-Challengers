959f4414f073f23995c13d93f02ad43e9652474f
Yuvaraj Dakhane <43665196+YuvarajDakhane@users.noreply.github.com>
2021-08-24 11:47:24 +0530

    Delete checkBalance.c

--- 3_Implementation/src/checkBalance.c
+++
@@ -1,9 +0,0 @@
-#include "bank.h"
-#include <stdio.h>
-
-void checkBalance(float balance)
-{
-    printf("You Choose to See your Balance\n");
-    printf("****Your Available Balance is:   $%.2f\n\n", balance);
-    
-}
