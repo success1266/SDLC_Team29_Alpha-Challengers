8dd06d46521493ec86c09ba2c14a38004b89db79
Ritesh0880 <65916282+Ritesh0880@users.noreply.github.com>
2021-08-24 11:55:48 +0530

    Add files via upload

---
+++ 3_Implementation/src/view_list.c
@@ -0,0 +1,41 @@
+#include "bank.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <stdbool.h>
+void view_list()
+{
+    int main_exit;
+    FILE *view;
+    view = fopen("record.dat", "r");
+    int test = 0;
+    fflush(stdin);
+    printf("\nACC. NO.\tNAME\t\t\tADDRESS\t\t\tPHONE\n");
+
+    while (fscanf(view, "%d %s %d/%d/%d %d %s %s %lf %s %f %d/%d/%d", &add.acc_no, add.name, &add.dob.month, &add.dob.day, &add.dob.year, &add.age, add.address, add.citizenship, &add.phone, add.acc_type, &add.amt, &add.deposit.month, &add.deposit.day, &add.deposit.year) != EOF)
+    {
+        printf("\n%6d\t %10s\t\t\t%10s\t\t%.0lf", add.acc_no, add.name, add.address, add.phone);
+        test++;
+    }
+
+    fclose(view);
+    if (test == 0)
+    {
+        fflush(stdin);
+        printf("\nNO RECORDS!!\n");
+    }
+
+view_list_invalid:
+    printf("\n\nEnter 1 to go to the main menu and 0 to exit:");
+    scanf("%d", &main_exit);
+    fflush(stdin);
+    if (main_exit == 1)
+        menu();
+    else if (main_exit == 0)
+        close();
+    else
+    {
+        printf("\nInvalid!\a");
+        goto view_list_invalid;
+    }
+}
