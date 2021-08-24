b0e34f4f7fdaae58cb0ad0370e75a54510b53d8f
Yuvaraj Dakhane <43665196+YuvarajDakhane@users.noreply.github.com>
2021-08-24 15:21:42 +0530

    Update menu.c

--- 3_Implementation/src/menu.c
+++ 3_Implementation/src/menu.c
@@ -1,19 +1,18 @@
 #include "bank.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
 #include <stdbool.h>
 void menu(void)
     {
     int choice;
-    system("cls");
+    fflush(stdin);
     system("color 7");
     printf("\n\n\t\t\t       BANKING MANAGEMENT SYSTEM");
     printf("\n\n\n\t\t\t\xB2\xB2\xB2\xB2\xB2\xB2\xB2 WELCOME TO THE MAIN MENU \xB2\xB2\xB2\xB2\xB2\xB2\xB2");
     printf("\n\n\t\t[1] Create a new account\n\t\t[2] Update information of existing account\n\t\t[3] Transactions\n\t\t[4] Check the details of existing account\n\t\t[5] Remove existing account\n\t\t[6] View customer's list\n\t\t[7] ATM Feature\n\t\t[8] Fund Transfer\n\t\t[9] Currency Exchange\n\t\t[10] Exit\n\n\n\t\t Enter your choice:");
     scanf("%d", &choice);
 
-    system("cls");
+    fflush(stdin);
     switch (choice)
         {
         case 1:
